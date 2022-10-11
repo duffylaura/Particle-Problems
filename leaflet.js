a377acc3b54ead35a9d6b9f515a52a94f80f1e55
Philip Huang <philiphuang2@gmail.com>
2022-10-11 14:10:47 -0700

    encountered an error with displaying the heatmap through leaflet

--- leaflet.js
+++ leaflet.js
@@ -1,6 +1,9 @@
+API_KEY = "f7ef38aba28849b8819220ae11e4b3e3";
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// https://tiles.breezometer.com/v1/pollen/grass/forecast/daily/{z}/{x}/{y}.png?key=' + API_KEY
+// 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+L.tileLayer('https://tiles.breezometer.com/v1/pollen/grass/forecast/daily/{z}/{x}/{y}.png?key=' + API_KEY, {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
