300bb6db4cd424accc1868becb7a1852ec47e919
NG <nicholeguan2@gmail.com>
2022-10-13 13:07:02 -0700

    set bounds to map to prevent user from accessing the map of world

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,10 @@
 API_KEY = "f7ef38aba28849b8819220ae11e4b3e3";
-var map = L.map('map').setView([28.538336, -81.379234], 13);
+var map = L.map('map', {minZoom: 10}).setView([28.538336, -81.379234], 13);
+
+var corner1 = L.latLng((28.538336-0.05), -81.379234-0.05),
+corner2 = L.latLng(28.538336+0.05, -81.379234+0.05),
+bounds = L.latLngBounds(corner1, corner2);
+map.setMaxBounds(bounds);
 //var mapEl = document.querySelector("#map");
 
 // https://tiles.breezometer.com/v1/pollen/grass/forecast/daily/{z}/{x}/{y}.png?key=' + API_KEY
