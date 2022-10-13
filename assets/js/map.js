ef3ec6a1fb9e3e53d05c379fe48ea0166e34bbf2
NG <nicholeguan2@gmail.com>
2022-10-13 14:54:05 -0700

    add event listeners to buttons so that overlay can be added to map but overlays stack when click on another button

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,8 @@
+var airPollutionBtnEl = document.querySelector("#airPollutionBtn");
+var grassBtnEl = document.querySelector("#grassPollenBtn");
+var treeBtnEl = document.querySelector("#grassPollenBtn");
+var weedBtnEl = document.querySelector("#grassPollenBtn");
+
 API_KEY = "f7ef38aba28849b8819220ae11e4b3e3";
 var map = L.map('map', {minZoom: 10}).setView([28.538336, -81.379234], 13);
 var pollenEndPoint = "https://api.breezometer.com/pollen/v2/forecast/daily?";
@@ -6,7 +11,6 @@ var corner1 = L.latLng((28.538336-0.05), -81.379234-0.05),
 corner2 = L.latLng(28.538336+0.05, -81.379234+0.05),
 bounds = L.latLngBounds(corner1, corner2);
 map.setMaxBounds(bounds);
-//var mapEl = document.querySelector("#map");
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -18,21 +22,44 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     .then((response) => response.json())
     .then((data) => console.log(data));*/
 
-var pollenType = "grass";
 
-L.tileLayer('https://tiles.breezometer.com/v1/pollen/' + pollenType +'/forecast/daily/{z}/{x}/{y}.png?key={key}', {
-    key: API_KEY,
-    maxZoom: 19,
-    tms: false,
-    opacity: 0.45,
-    attribution: '&copy; <a href="https://www.breezometer.com/terms-of-use">Breezometer</a>'
-}).addTo(map);
-
+airPollutionBtnEl.addEventListener('click', function(){
 //air quality overlay
-/*L.tileLayer('https://tiles.breezometer.com/v1/air-quality/pm25/current-conditions/{z}/{x}/{y}.png?key={key}', {
-    key: API_KEY,
-    maxZoom: 19,
-    tms: false,
-    opacity: 0.45,
-    attribution: '&copy; <a href="https://www.breezometer.com/terms-of-use">Breezometer</a>'
-}).addTo(map);*/
+    L.tileLayer('https://tiles.breezometer.com/v1/air-quality/pm25/current-conditions/{z}/{x}/{y}.png?key={key}', {
+        key: API_KEY,
+        maxZoom: 19,
+        tms: false,
+        opacity: 0.45,
+        attribution: '&copy; <a href="https://www.breezometer.com/terms-of-use">Breezometer</a>'
+    }).addTo(map)
+});
+
+grassBtnEl.addEventListener('click', function(){
+    L.tileLayer('https://tiles.breezometer.com/v1/pollen/grass/forecast/daily/{z}/{x}/{y}.png?key={key}', {
+        key: API_KEY,
+        maxZoom: 19,
+        tms: false,
+        opacity: 0.45,
+        attribution: '&copy; <a href="https://www.breezometer.com/terms-of-use">Breezometer</a>'
+    }).addTo(map);
+});
+
+treeBtnEl.addEventListener('click', function(){
+    L.tileLayer('https://tiles.breezometer.com/v1/pollen/tree/forecast/daily/{z}/{x}/{y}.png?key={key}', {
+        key: API_KEY,
+        maxZoom: 19,
+        tms: false,
+        opacity: 0.45,
+        attribution: '&copy; <a href="https://www.breezometer.com/terms-of-use">Breezometer</a>'
+    }).addTo(map);
+});
+
+weedBtnEl.addEventListener('click', function(){
+    L.tileLayer('https://tiles.breezometer.com/v1/pollen/weed/forecast/daily/{z}/{x}/{y}.png?key={key}', {
+        key: API_KEY,
+        maxZoom: 19,
+        tms: false,
+        opacity: 0.45,
+        attribution: '&copy; <a href="https://www.breezometer.com/terms-of-use">Breezometer</a>'
+    }).addTo(map);
+});
