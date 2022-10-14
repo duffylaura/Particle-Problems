6aba78951a2f9d1bf12475faebf8442ef114f76f
Coffee Pawz <lauralynneduffy@gmail.com>
2022-10-14 12:35:42 -0400

    Form completed; need to do local storage

--- assets/js/action-plan.js
+++ assets/js/action-plan.js
@@ -16,11 +16,6 @@ var handleFormSubmit = function (event) {
 
   var doingWell = doingWellEl.val();
 
-  if (!doingWell) {
-    console.log('You need to fill out the form!');
-    return;
-  }
-
   printDoingWell(doingWell);
 
   doingWellEl.val('');
@@ -30,3 +25,53 @@ var handleFormSubmit = function (event) {
 formEl.on('submit', handleFormSubmit);
 
 ///////////////////////
+
+// Asthma Getting Worse //
+
+var gettingWorseFormEl = $('#getting-worse-form');
+var gettingWorseEl = $('#getting-worse');
+var gettingWorseListEl = $('#getting-worse-list');
+
+var printGettingWorse = function (name) {
+  var gwlistEl = $('<li>');
+  gwlistEl.addClass('getting-worse-list-group-item').text(name);
+  gwlistEl.appendTo(gettingWorseListEl);
+};
+
+var handleGettingWorseFormSubmit = function (event) {
+  event.preventDefault();
+
+  var gettingWorse = gettingWorseEl.val();
+
+  printGettingWorse(gettingWorse);
+
+  gettingWorseEl.val('');
+  
+};
+
+gettingWorseFormEl.on('submit', handleGettingWorseFormSubmit);
+
+// Medical Alert //
+
+var medicalAlertFormEl = $('#medical-alert-form');
+var medicalAlertEl = $('#medical-alert');
+var medicalAlertListEl = $('#medical-alert-list');
+
+var printMedicalAlert = function (name) {
+  var malistEl = $('<li>');
+  malistEl.addClass('medical-alert-list-group-item').text(name);
+  malistEl.appendTo(medicalAlertListEl);
+};
+
+var handleMedicalAlertFormSubmit = function (event) {
+  event.preventDefault();
+
+  var medicalAlert = medicalAlertEl.val();
+
+  printMedicalAlert(medicalAlert);
+
+  medicalAlertEl.val('');
+  
+};
+
+medicalAlertFormEl.on('submit', handleMedicalAlertFormSubmit);
