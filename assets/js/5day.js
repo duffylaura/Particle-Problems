c4a1f62303a8300324ac3620299a47339d14886d
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-14 09:25:46 -0700

    Create localstorage of data collected on index2.html then use it 5day.html

--- assets/js/5day.js
+++ assets/js/5day.js
@@ -1,4 +1,31 @@
-console.log("hello world");
+// console.log("hello world");
+// console.log(fiveDay)
 
-var indexsubmitbtn = document.getElementById("form")
-console.log("help!")
+
+// indexsubmitbtn.addEventListener("submit", function(help){
+//     help.preventDefault();
+//     if(document.getElementById('five-day').checked){
+//         console.log("this button is checked")
+//         window.location.href = "5day.html"
+//     }
+//     console.log("help");
+
+// function createCards(data){
+//     console.log(data)
+//     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
+//     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
+//     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
+// //     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
+// //     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
+// //     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
+// }
+
+
+// import {getWeatherAPI} from "./script2.js";
+// console.log(getWeatherAPI);
+
+var airindex = document.getElementById("air-index")
+
+var air = localStorage.getItem("AQI");
+airindex.append(air);
+
