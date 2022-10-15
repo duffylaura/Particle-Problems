23e9f76e130176f263e2dc47602dad1f414faddc
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-14 21:47:05 -0700

    create a successful connection from openweathermap api and from breezometer api. to display so we can pull data from the console

--- assets/js/5day.js
+++ assets/js/5day.js
@@ -7,31 +7,31 @@ airindex.append(air);
 var carddate = localStorage.getItem("UNIX"); 
 cardheaderdate.append(carddate);
 
-var breezokey = "f7ef38aba28849b8819220ae11e4b3e3";
+var saveCity = localStorage.getItem("cityName");
 
-function geoCodeApi (search){
-    console.log(search)
-    var geocodeKey = 'f4ac9ae98ce232f81e1a8c7e3fd76a5a';
-    var url = 'https://api.openweathermap.org/geo/1.0/direct?q='+search+'&limit=1&appid='+geocodeKey
-    console.log(url)
+function geoCode5day (){
+    var geocodeKey = "f4ac9ae98ce232f81e1a8c7e3fd76a5a";
+    var url = "https://api.openweathermap.org/geo/1.0/direct?q="+saveCity+"&limit=1&appid="+geocodeKey
     fetch(url).then(function(response){
+        return response.json()
+    }).then(function(data){
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        breezometer(lat,lon)
+    })
+}
+        
+    function breezometer(lat,lon){
+    var breezokey = "f7ef38aba28849b8819220ae11e4b3e3";
+    url= "https://api.breezometer.com/pollen/v2/forecast/daily?lat="+lat+"&lon="+lon+"&days=3&key="+breezokey
+    fetch(url).then(function(response){ 
         console.log(response)
         return response.json()
     }).then(function(data){
         console.log(data);
-        // console.log([0]);
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        console.log(lat)
-        breezometer(lat,lon)
-    })}
-        
-    function breezometer(lat,lon){
-    fetch("https://api.breezometer.com/pollen/v2/forecast/daily?"+lat+"&"+lon+"&5=3&"+breezokey).then(function(response){ 
-        return response.json()
-    }).then(function(data){
-        console.log(data)
         
     })
 
     }
+
+    geoCode5day()
