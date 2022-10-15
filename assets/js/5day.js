eea96b569a63ee513bee144be9af7ea192315d73
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-14 21:21:52 -0700

    input breezometer API into 5-day forecast js.

--- assets/js/5day.js
+++ assets/js/5day.js
@@ -1,29 +1,3 @@
-// console.log("hello world");
-// console.log(fiveDay)
-
-
-// indexsubmitbtn.addEventListener("submit", function(help){
-//     help.preventDefault();
-//     if(document.getElementById('five-day').checked){
-//         console.log("this button is checked")
-//         window.location.href = "5day.html"
-//     }
-//     console.log("help");
-
-// function createCards(data){
-//     console.log(data)
-//     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
-//     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
-//     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
-// //     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
-// //     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
-// //     // airElement.textContent = "Concentration of CO (Carbon monoxide): " + data.components.co + "μg/m3";
-// }
-
-
-// import {getWeatherAPI} from "./script2.js";
-// console.log(getWeatherAPI);
-
 var airindex = document.getElementById("air-index")
 var cardheaderdate = document.getElementById("card-date")
 
@@ -33,4 +7,31 @@ airindex.append(air);
 var carddate = localStorage.getItem("UNIX"); 
 cardheaderdate.append(carddate);
 
+var breezokey = "f7ef38aba28849b8819220ae11e4b3e3";
 
+function geoCodeApi (search){
+    console.log(search)
+    var geocodeKey = 'f4ac9ae98ce232f81e1a8c7e3fd76a5a';
+    var url = 'https://api.openweathermap.org/geo/1.0/direct?q='+search+'&limit=1&appid='+geocodeKey
+    console.log(url)
+    fetch(url).then(function(response){
+        console.log(response)
+        return response.json()
+    }).then(function(data){
+        console.log(data);
+        // console.log([0]);
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        console.log(lat)
+        breezometer(lat,lon)
+    })}
+        
+    function breezometer(lat,lon){
+    fetch("https://api.breezometer.com/pollen/v2/forecast/daily?"+lat+"&"+lon+"&5=3&"+breezokey).then(function(response){ 
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        
+    })
+
+    }
