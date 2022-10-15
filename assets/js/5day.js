c9cadec93a050fbbcf7a5c855d47515d09c60526
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-15 13:29:06 -0700

    create a for loop to generate data three times according to their data points that are received from the api locate in the console. I also create new divs and append new variables to 5day.html

--- assets/js/5day.js
+++ assets/js/5day.js
@@ -1,25 +1,24 @@
 var airindex = document.getElementById("air-index")
 var cardheaderdate = document.getElementById("card-date")
-
 var air = localStorage.getItem("AQI");
-airindex.append(air);
-
 var carddateUnix = localStorage.getItem("UNIX"); 
-var correctDate = moment.unix(carddateUnix).format("MM/DD/YYYY");
-cardheaderdate.append(correctDate);
-
+var correctDate = moment.unix(carddateUnix).format("dddd MM/DD/YYYY");
 var saveCity = localStorage.getItem("cityName")
 var uppersaveCity = saveCity.toUpperCase();
+// var lat2 = localStorage.getItem("lat");
+// var lon2 = localStorage.getItem("lon");
+var mainHeader = document.getElementById("container-header");
 
-var lat2 = localStorage.getItem("lat");
-var lon2 = localStorage.getItem("lon");
 
-geoCode5day()
+// airindex.append(air);
+var cardBody = document.getElementById("card-body")
+cardheaderdate.append(correctDate);
+mainHeader.append(uppersaveCity + " 3 Day Forecast");
+
 
 function geoCode5day (){
     var geocodeKey = "f4ac9ae98ce232f81e1a8c7e3fd76a5a";
     var url = "https://api.openweathermap.org/geo/1.0/direct?q="+saveCity+"&limit=1&appid="+geocodeKey
-    console.log(saveCity)
     fetch(url).then(function(response){
         return response.json()
     }).then(function(data){
@@ -38,22 +37,38 @@ function breezometer(lat,lon){
         console.log(response)
         return response.json()
     }).then(function(data){
-        console.log(data);
-    })
-}
-
-function currentCity(){
-    var apiKey = "aa3ac1aee36fc947283c79786b233621";
-    var url = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat2}&lon=${lon2}&appid=${apiKey}`
-    fetch(url).then(function(response){
-        console.log(response)
-        return response.json()
-    }).then(function(data){
         console.log(data)
-        // data = data.list[0]
-        // createCards(data)
-        })
-    }
+        
+    var apiKey = "aa3ac1aee36fc947283c79786b233621";
+    var oneweatherairurl = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    fetch(oneweatherairurl).then(function(pollutants){
+        console.log(pollutants)
+        return pollutants.json()
+    }).then(function(pollutants){
+        console.log(pollutants)
+
+        for(var i = 0; i < 3; i++){
+            var bodyNewMainDiv = document.createElement("div");
+            bodyNewMainDiv.setAttribute("class", "body-new-div")
+            var cards = document.createElement("div");
+            cards.setAttribute("class", "all-data-body");
+            var h4 = document.createElement("h4").textContent = moment(data.data[i].date).format("MM/DD/YYYY");
+            // var icon = document.createElement("img"); if we want to add in image
+            // icon.setAttribute("src", "" + newImageIcon + ".png"); if we want to add in image
+            var newaqi = document.createElement("p").textContent = "AQI: "+pollutants.list[i].main.aqi;
+            var newgrasspollen = document.createElement("p").textContent = "Grass Pollen Index: "+data.data[i].types.grass.index;
+            var newtreepollen = document.createElement("p").textContent = "Tree Pollen Index: "+data.data[i].types.tree.index;
+            var newweedpollen = document.createElement("p").textContent = "Weed Pollen Index: "+data.data[i].types.weed.index;
+            cards.append(h4, newaqi, newgrasspollen, newtreepollen, newweedpollen);
+            bodyNewMainDiv.append(cards);
+            cardBody.append(bodyNewMainDiv); 
+  
+        }})})}
     
-    currentCity()
+// }})
+
+// }
+
+    geoCode5day()
+
 
